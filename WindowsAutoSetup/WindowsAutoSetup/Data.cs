0c806c795c3d2cdded0079f8e586d99e901f13ff
James Dearlove <jamesdearlove@outlook.com>
2019-08-18 22:15:30 +1000

    Code setup, Mahapps & NewtonSoft installed

---
+++ WindowsAutoSetup/WindowsAutoSetup/Data.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsAutoSetup.Data
+{
+    public class Branding
+    {
+        public string name { get; set; }
+        public string logo { get; set; }
+    }
+
+    public class DomainInfo
+    {
+        public string name { get; set; }
+        public object username { get; set; }
+        public object password { get; set; }
+        public bool reboot { get; set; }
+    }
+
+    public class Application
+    {
+        public string name { get; set; }
+        public string command { get; set; }
+        public bool reboot { get; set; }
+        public bool beforeDomain { get; set; }
+        public int order { get; set; }
+        public string download { get; set; }
+    }
+
+    public class RootObject
+    {
+        public int version { get; set; }
+        public Branding branding { get; set; }
+        public DomainInfo domainInfo { get; set; }
+        public List<Application> applications { get; set; }
+        public List<string> order { get; set; }
+    }
+}
---
+++ WindowsAutoSetup/WindowsAutoSetup/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using WindowsAutoSetup.Data;
+using MahApps.Metro.Controls;
+
+
+namespace WindowsAutoSetup
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+
+        private RootObject data;
+
+        private DomainInfo domainInfo;
+
+        public String domainString { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // read file into a string and deserialize JSON to a type
+            data = JsonConvert.DeserializeObject<RootObject>(File.ReadAllText(@"C:\Users\james\Desktop\example.json"));
+            domainInfo = data.domainInfo;
+            domainString = domainInfo.name;
+            MessageBox.Show(domainInfo.ToString());
+        }
+    }
+}
